d08b6d9c0188765846cded71fb2ee85181ada4ea
Alexander Popelyuk <alexander@popelyuk.net>
2018-04-30 21:54:05 +0300

    Added Newton package

---
+++ Task_1/Sources/OldClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Task_1.OldClient
+{
+    [XmlRoot("cl")]
+    public class Client
+    {
+        [XmlElement("fn")]
+        public string FirstName;
+        [XmlElement("ln")]
+        public string LastName;
+        [XmlElement("mn")]
+        public string MiddleName;
+        [XmlElement("p")]
+        public string PhoneNumber;
+        [XmlElement("e")]
+        public string Email;
+        [XmlElement("bd")]
+        public byte BirthDay;
+        [XmlElement("bm")]
+        public byte BirthMonth;
+        [XmlElement("by")]
+        public uint BirthYear;
+        [XmlElement("hl1")]
+        public string HomeAddressLine1;
+        [XmlElement("hc")]
+        public string HomeAddressCity;
+        [XmlElement("hs")]
+        public string HomeAddressState;
+        [XmlElement("hz")]
+        public string HomeAddressZip;
+        [XmlElement("wl1")]
+        public string WorkAddressLine1;
+        [XmlElement("wc")]
+        public string WorkAddressCity;
+        [XmlElement("ws")]
+        public string WorkAddressState;
+        [XmlElement("wz")]
+        public string WorkAddressZip;
+    }
+
+
+    class OldClient
+    {
+    }
+}
