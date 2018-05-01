bd753dc468287b033899cceda65a75b97a1e44a3
Alexander Popelyuk <alexander@popelyuk.net>
2018-05-01 21:17:52 +0300

    Added 'Operation' class definition.

---
+++ Task_2/Sources/Operation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+
+namespace Task_2
+{
+    class Operation
+    {
+        public enum Type
+        {
+            [XmlEnum(Name = "income")]
+            Debit,
+            [XmlEnum(Name = "expense")]
+            Credit,
+        }
+
+        [XmlAttribute("type")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Type OperationType;
+        public Decimal Amount;
+        public DateTime Date;
+    }
+}
--- Task_2/Sources/Program.cs
+++ Task_2/Sources/Program.cs
@@ -8,10 +8,60 @@ namespace Task_2
 {
     class Program
     {
+        const long MaxInputSize = 5 * 1024 * 1024;
+        const string DefaultSamplesDirectory = ".\\operations";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("task 2");
-            Console.ReadKey(true);
+            try
+            {
+                string SamplesDirectory = DefaultSamplesDirectory;
+
+                if (args.Length > 1)
+                {
+                    PrintError("Too many arguments.");
+                    PrintUsage();
+                }
+                else if (args.Length == 1 && args[0] == "/?") PrintHelp();
+                else
+                {
+                    if (args.Length > 0) SamplesDirectory = args[0];
+                    ProcessData(SamplesDirectory);
+                }
+            }
+            catch (Exception ex)
+            {
+                PrintError(ex.Message);
+            }
+            #if DEBUG
+                Console.ReadKey(true);
+            #endif
+        }
+
+        static void ProcessData(string samples_directory)
+        {
+            Console.WriteLine("processing data... ok");
+        }
+
+        static void PrintHelp()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Process operations data and print statistics.");
+            PrintUsage();
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine();
+            Console.WriteLine("USAGE: Task_2.exe [ input ]");
+            Console.WriteLine();
+            Console.WriteLine("input\tInput folder with samples to process (default: {0}).", DefaultSamplesDirectory);
+        }
+
+        static void PrintError(string text)
+        {
+            Console.WriteLine();
+            Console.WriteLine("ERROR: {0}", text);
         }
     }
 }
